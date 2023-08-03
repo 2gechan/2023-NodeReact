8ef0083710268e1ee2bad55c65d8e072da716d9d
2gechan <7532602@gmail.com>
2023-08-03 12:51:55 +0900

    useState_component_전달

---
+++ react-001-todolist-01/src/comps/TodoMain.jsx
@@ -0,0 +1,36 @@
+import TodoInput from "./TodoInput";
+import { useState } from "react";
+import "../css/Todo.css";
+import TodoList from "./TodoList";
+
+import { SampleData } from "../data/SampleTodo";
+
+const TodoMain = () => {
+  /**
+   * State 끌어 올리기
+   * TodoInput과 TodoList에 있던 state 들을
+   * TodoMain 부모 컴포넌트로 이동
+   * 1. TodoInput 에서 입력된 content state의 값을
+   * 2. TodoList 의 todoList state 배열에 추가하여
+   * 3. TodoList > TodoItem을 통하여 화면에 출력해야 한다.
+   *
+   * 이 상황에서 TodoInput과 TodoList는 같은 부모의 형제간이다.
+   *    React 에서는 형제간에 state를 절대 공유할 수 없다.
+   *    React 부모가 만들어서 전달해준 State 만 볼 수 있다
+   *    자식이 만든 State는 부모도 볼 수 없다
+   *
+   * 이 상황을 해결하기 위하여 자식 Comps 있던 state와 state 함수를
+   * 부모 Comps인 TodoMain 으로 끌어 올리기를 한다.
+   * 그리고, 자식 Comps 에 전달해주어야 한다.
+   */
+  const [content, setContent] = useState("");
+  const [todoList, setTodoList] = useState([...SampleData]);
+  return (
+    <div className="todo">
+      <TodoInput content={content} setContent={setContent} />
+      <TodoList todoList={todoList} />
+    </div>
+  );
+};
+
+export default TodoMain;
