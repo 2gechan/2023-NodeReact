2608720c449118e7230eb5526aaa788f811d81e5
2gechan <7532602@gmail.com>
2023-08-10 16:13:51 +0900

    props Drilling

--- Node-003-bbs-v3/react-client/src/comps/BBsInput.jsx
+++ Node-003-bbs-v3/react-client/src/comps/BBsInput.jsx
@@ -1,19 +1,27 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { filePreview, filesPreview } from "../modules/ImagePreview";
-import { bbsInsert } from "../modules/FetchModules";
+
 import css from "../css/BBsInput.module.css";
+import { useBBsContext } from "../provide/BBsProvider";
 
 const BBsInput = () => {
-  const [bbs, setBBs] = useState({
-    b_seq: 0,
-    b_nickname: "",
-    b_title: "",
-    b_content: "",
-  });
+  /**
+   * BBsProvider Store에 보관되어 있는 bbs와 setBbs() 함수를 가져와서 사용해야 하는데
+   * 그 함수를 가져오기 위하여 useContext() 라는 Hook 함수를 사용한다.
+   * useContext() 함수는 어떤 스토어에서 값들을 가져오는지 명시 해주어야 한다.
+   * 그런데 BBsInput에서는 스토어에 이름, 키 등을 알 방법이 없다.
+   * 알아낼 방법은 있지만 너무 많은 코드가 필요하다.
+   * 그래서 BBsProvider에서는 자신으 스토어 정보를 포함한 사용자 정의 useContext()를
+   * 만들어 두었다.
+   */
+
+  // const {bbs, setBbs} = useContext();
+  const { bbs, setBBs, bbsInsertCB, imgRef, imgsRef } = useBBsContext();
+
   const [image, setImage] = useState("");
   const [images, setImages] = useState([]);
-  const imgRef = useRef(null);
-  const imgsRef = useRef(null);
+  // const imgRef = useRef(null);
+  // const imgsRef = useRef(null);
 
   const setMainImage = (image) => {
     setImage(image);
@@ -55,35 +63,32 @@ const BBsInput = () => {
    * 3. fetch 보내기
    */
   const insertButtonClickHandler = async (e) => {
-    // alert("hello");
-    // js에서 제공하는 Http 객체다
-    const formData = new FormData();
-    const file = imgRef?.current.files[0];
-    const files = imgsRef.current.files;
-
-    // formData에 bbs(JSON 객체)를 실어서 서버로 보낼때는
-    // 객체를 직접 보낼 수 없다.
-    // 객체를 Serialize(직렬화, 문자열화)
-    const bbsStr = JSON.stringify(bbs);
-
-    // node의 router Upload 미들웨어에서 받을 이름에
-    // 모든 파일 정보를 append()
-    // 대표이미지
-    formData.append("b_images", file);
-    // 갤러리 이미지들
-    for (let file of files) {
-      formData.append("b_images", file);
-    }
-    formData.append("bbs", bbsStr);
-
-    // formData.append("bbs", bbs);
-    // document.querySelector("#b_img").files[0] 와 같다.
-    // formData.append("b_img", imgRef.current.files[0]);
-    // formData.append("b_title", bbs.b_title);
-    // formData.append("b_nickname", bbs.b_nickname);
-    // formData.append("b_content", bbs.b_content);
-
-    await bbsInsert(formData);
+    bbsInsertCB();
+    // // alert("hello");
+    // // js에서 제공하는 Http 객체다
+    // const formData = new FormData();
+    // const file = imgRef?.current.files[0];
+    // const files = imgsRef.current.files;
+    // // formData에 bbs(JSON 객체)를 실어서 서버로 보낼때는
+    // // 객체를 직접 보낼 수 없다.
+    // // 객체를 Serialize(직렬화, 문자열화)
+    // const bbsStr = JSON.stringify(bbs);
+    // // node의 router Upload 미들웨어에서 받을 이름에
+    // // 모든 파일 정보를 append()
+    // // 대표이미지
+    // formData.append("b_images", file);
+    // // 갤러리 이미지들
+    // for (let file of files) {
+    //   formData.append("b_images", file);
+    // }
+    // formData.append("bbs", bbsStr);
+    // // formData.append("bbs", bbs);
+    // // document.querySelector("#b_img").files[0] 와 같다.
+    // // formData.append("b_img", imgRef.current.files[0]);
+    // // formData.append("b_title", bbs.b_title);
+    // // formData.append("b_nickname", bbs.b_nickname);
+    // // formData.append("b_content", bbs.b_content);
+    // await bbsInsert(formData);
   };
 
   return (
--- Node-003-bbs-v3/react-client/src/comps/BBsList.jsx
+++ Node-003-bbs-v3/react-client/src/comps/BBsList.jsx
@@ -1,15 +1,16 @@
-import { NavLink, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useBBsContext } from "../provide/BBsProvider";
 import css from "../css/BBsList.module.css";
 
+const sampleData = [
+  { b_seq: 0, b_nickname: "홍길동", b_title: "게시판1" },
+  { b_seq: 1, b_nickname: "이몽룡", b_title: "게시판2" },
+  { b_seq: 2, b_nickname: "성춘향", b_title: "게시판3" },
+  { b_seq: 3, b_nickname: "임꺽정", b_title: "게시판4" },
+  { b_seq: 4, b_nickname: "엄복동", b_title: "게시판5" },
+];
+
 const BBsList = () => {
-  const [bbsList, setBbsList] = useState([
-    { b_seq: 0, b_nickname: "홍길동", b_title: "게시판1" },
-    { b_seq: 1, b_nickname: "이몽룡", b_title: "게시판2" },
-    { b_seq: 2, b_nickname: "성춘향", b_title: "게시판3" },
-    { b_seq: 3, b_nickname: "임꺽정", b_title: "게시판4" },
-    { b_seq: 4, b_nickname: "엄복동", b_title: "게시판5" },
-  ]);
+  const { bbsList, setBBsList } = useBBsContext();
 
   const bbsItems = bbsList.map((bbs) => {
     return (
@@ -21,21 +22,7 @@ const BBsList = () => {
       </tr>
     );
   });
-  const navigate = useNavigate();
-  const tableClickHandler = (e) => {
-    const target = e.target;
-    // alert(target.tagName);
-    // target의 부모 중에서 가장 가까이 있는 TR이 누구인지 찾는다.
-    const tr = target.closest("TR");
-    // data-seq로 설정한 데이터 가져오기
-    // alert(tr.dataset.seq);
-    navigate(`/bbs/detail/${tr.dataset.seq}`);
-  };
-  /**
-   * JS의 join() 함수 : 배열의 요소를 하나의 문자열로 변환하는 함수
-   * const arr = [1,2,3,4,5]
-   * arr.join(" ") => "1 2 3 4 5"
-   */
+
   return (
     <>
       <table className={css.main}>
@@ -47,13 +34,8 @@ const BBsList = () => {
             <th>조회수</th>
           </tr>
         </thead>
-        <tbody onClick={tableClickHandler}>{bbsItems}</tbody>
+        <tbody>{bbsItems}</tbody>
       </table>
-      <div className={css.main}>
-        <NavLink to="/bbs/insert" className={[css.link, css.hover].join(" ")}>
-          추가하기
-        </NavLink>
-      </div>
     </>
   );
 };
