c88d712e124ad977d26e7722cd0bf84531a5296d
2gechan <7532602@gmail.com>
2023-08-10 14:12:49 +0900

    react router

--- Node-003-bbs-v2/react-client/Router.md
+++ Node-003-bbs-v2/react-client/Router.md
@@ -4,3 +4,11 @@
 - 도구 설치 : `npm install react-router-dom`
 - react-router-dom 은 5.x 버전과 6.x 버전은 호환되지 않는다.
 - 6.x 버전에서는 Navigation 설정 부분이 새롭게 만들어지고, Menu 만들기가 매우 쉬워졌다.
+
+## react-router-dom을 이용한 Menu 생성
+
+- `NavList.jsx` 파일에서 화면에 보여질 Menu를 구현했다
+- `NavLink` 컴포넌트를 사용하여 각 a Link를 구현
+- `MainRouter.js`(Navigation Provider) 에서 Link가 클릭되었을 때 보여질 화면의 layout을 구현한다.
+- 최상단의 index.js에 부착하고 App.js를 최 상단 layout 컴포넌트로 구현
+- App.js에는 `<Outlet/>` 컴포넌트가 위치하고 여기에 메뉴를 클릭했을 때 보여질 컴포넌트들이 위치하게 된다.
--- Node-003-bbs-v2/react-client/src/comps/BBsList.jsx
+++ Node-003-bbs-v2/react-client/src/comps/BBsList.jsx
@@ -1,5 +1,6 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import css from "../css/BBsList.module.css";
 
 const BBsList = () => {
   const [bbsList, setBbsList] = useState([
@@ -20,18 +21,40 @@ const BBsList = () => {
       </tr>
     );
   });
+  const navigate = useNavigate();
+  const tableClickHandler = (e) => {
+    const target = e.target;
+    // alert(target.tagName);
+    // target의 부모 중에서 가장 가까이 있는 TR이 누구인지 찾는다.
+    const tr = target.closest("TR");
+    // data-seq로 설정한 데이터 가져오기
+    // alert(tr.dataset.seq);
+    navigate(`/bbs/detail/${tr.dataset.seq}`);
+  };
+  /**
+   * JS의 join() 함수 : 배열의 요소를 하나의 문자열로 변환하는 함수
+   * const arr = [1,2,3,4,5]
+   * arr.join(" ") => "1 2 3 4 5"
+   */
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>SEQ</th>
-          <th>작성자</th>
-          <th>제목</th>
-          <th>조회수</th>
-        </tr>
-      </thead>
-      <tbody>{bbsItems}</tbody>
-    </table>
+    <>
+      <table className={css.main}>
+        <thead>
+          <tr>
+            <th>SEQ</th>
+            <th>작성자</th>
+            <th>제목</th>
+            <th>조회수</th>
+          </tr>
+        </thead>
+        <tbody onClick={tableClickHandler}>{bbsItems}</tbody>
+      </table>
+      <div className={css.main}>
+        <NavLink to="/bbs/insert" className={[css.link, css.hover].join(" ")}>
+          추가하기
+        </NavLink>
+      </div>
+    </>
   );
 };
 
